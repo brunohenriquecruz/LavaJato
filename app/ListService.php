37711345b2b50823da798fddfe6635e32dddf709
Gabriel Alcantara <gabrielalcantaratp@gmail.com>
2019-06-30 22:08:45 -0300

    *Tirando function desnecessária*

--- app/ListService.php
+++ app/ListService.php
@@ -10,11 +10,4 @@ class ListService extends Model
     protected $fillable = ['id_client', 'id_car', 'id_service'];
     protected $guarded = ['id', 'created_at'];
     protected $table = 'washes';
-
-    public function mostrar($id)
-    {
-        $query = DB::table('washes')->select(DB::raw('washes.id, washes.id_client'))
-                                    ->get();
-        return $query;
-    }
 }
